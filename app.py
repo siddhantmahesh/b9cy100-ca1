239e8f62089600d9087cd5375d4e2bde18bf75ec
siddhantmahesh <92044233+siddhantmahesh@users.noreply.github.com>
2021-12-01 17:21:17 +0000

    added code to read and create hours object

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-import datetime
+class Hours:
+
+    def __init__(self, empID:str, dateWorked:str, hoursWorked:float):
+
+        self.empID = empID
+        self.dateWorked = dateWorked
+        self.hoursWorked = hoursWorked
 
 class Employee:
 
     def __init__(self, empID:str, lName:str, fName:str, regHours:float, hourlyRate:float,
      otMultiple:float, taxCredit:float, standardBand:float):
+
         self.empID = empID
         self.lName = lName
         self.fName = fName
@@ -15,6 +22,7 @@ class Employee:
 
 
     def computePayment(self, hoursWorked:float, dateWorked:str):
+
         regularHrsWorked = 0
         regularPay = 0
         otHrsWorked = 0
@@ -74,27 +82,74 @@ class Employee:
 ############################## READ FROM EMPLOYEE AND HOURS FILE ########################################
 
 empObjectList = []
+hrsObjectDict = {}
 
 with open("employees.txt", "r") as emp:
 
     empList = []
-    for item in emp:
-        empList.append(item)
-        print(str(empList))
 
-    for item in empList:
-        empDetails = item.split()
+    for item in emp: #line in employees.txt
+
+        empList.append(item)
+        #print(str(empList))  ['BAT1536 WAYNE BRUCE 40 25 1.5 80 750\n', 'MAN1331 MAN BAT 35 45 1.5 80 700']
+
+    for item in empList: #field line in above array
+
+        empDetails = item.split() 
+        #print(str(empDetails)) [BAT1536, WAYNE, BRUCE, 40, 25, 1.5, 80, 750\n]
 
         empObject = Employee(empDetails[0],empDetails[1],empDetails[2]
             ,empDetails[3],empDetails[4],empDetails[5],empDetails[6],empDetails[7])
+        #Employee object CTR:
+        #Employee(empID: str, lName: str, fName: str, regHours: float, 
+                #hourlyRate: float, otMultiple: float, taxCredit: float, standardBand: float)
 
-        empObjectList.append(empObject)
-        #print(str(empDetails))
+        empObjectList.append(empObject) #add employee object to emp object list
 
-    #print(str(empObjectList))
-    for item in empObjectList:
-        print(str(item.fName))
+    # for item in empObjectList:
+    #     print(str(item.fName))
     
+
+with open("hours.txt", "r") as hrs:
+    
+    hoursList = []
+
+    for item in hrs: #line in hours.txt
+        lis = item.split()
+        hoursList.append(lis)
+        # print(item) => BAT1536 01/01/2021 40
+
+    for item in hoursList:
+        empID = item[0]
+        if empID not in hrsObjectDict.keys():
+            hrsObjectDict[empID] = []
+
+    for item in hoursList:
+        empID = item[0]
+        hrsObjectDict[empID].append(Hours(item[0], item[1], item[2]))
+
+    print(str(hrsObjectDict))
+    # print(str(hoursList)) => [['BAT1536', '01/01/2021', '40'], ['BAT1536', '08/01/2021', '35'],...]
+            
+        # print(str(item))
+
+
+        
+        # empID = item.split(" ")[0]
+        # # print(empID)
+        # # print("empID")
+
+        # if empID not in hrsObjectDict.keys():
+        #     print(str(lis))
+        #     print("here")
+        #     val = []
+        #     hrsObjectDict[empID] = val.append(item.split())
+        
+        # else:
+        #     print(str(lis))
+        #     print("there")
+        #     val = []
+        #     hrsObjectDict[empID] = val.append(item.split())
 
 
 
